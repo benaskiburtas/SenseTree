2fe09b8fe36b56a12372efe7c9f4a49061b34589
Benas Kiburtas <benas.kiburtas@pm.me>
2024-04-11 19:15:09 +0300

    Relocate missing constants

--- addons/sensetree/common/constants.gd
+++ addons/sensetree/common/constants.gd
@@ -2,3 +2,9 @@ extends Node
 
 enum ProcessMode { IDLE, PHYSICS }
 enum NodeGroup { TREE, COMPOSITE, DECORATOR, LEAF }
+
+
+const TREE_GROUP_BASE_COLOR = Color("#19782a")
+const COMPOSITE_GROUP_BASE_COLOR = Color("#193e78")
+const DECORATOR_GROUP_BASE_COLOR = Color("#571978")
+const LEAF_GROUP_BASE_COLOR = Color("#784819")
--- addons/sensetree/tree_visualizer/scripts/tree_graph_node.gd
+++ addons/sensetree/tree_visualizer/scripts/tree_graph_node.gd
@@ -2,10 +2,11 @@
 class_name TreeVisualizerGraphNode
 extends GraphNode
 
+enum PortType { INPUT, OUTPUT }
 enum AlignmentType { VERTICAL, HORIZONTAL }
 
-const INPUT_PORT_TYPE = SenseTreeConstants.GraphNodePortType.INPUT
-const OUTPUT_PORT_TYPE = SenseTreeConstants.GraphNodePortType.OUTPUT
+const INPUT_PORT_TYPE = PortType.INPUT
+const OUTPUT_PORT_TYPE = PortType.OUTPUT
 const DEFAULT_SLOT_INDEX: int = 0
 
 const MINIMUM_WIDTH: int = 50
@@ -36,20 +37,20 @@ func _init(
 	alignment_mode: AlignmentType = AlignmentType.HORIZONTAL
 ) -> void:
 	var sense_node = arranged_node.tree
-	
+
 	_alignment_mode = alignment_mode
 	_set_base_properties()
 	_initialize_port()
 	_initialize_content_vbox()
 	_initialize_titlebar()
 	_initialize_titlebar_icon()
-	
+
 	_configure_ports(sense_node)
-	_load_node_icon(sense_node)	
+	_load_node_icon(sense_node)
 	_load_node_title(sense_node)
 	_load_node_properties(sense_node)
 	_assign_styleboxes_by_group(sense_node, style_boxes)
-	
+
 	_set_node_position(arranged_node)
 
 
@@ -57,9 +58,11 @@ func _set_base_properties() -> void:
 	custom_minimum_size = Vector2(MINIMUM_WIDTH, MINIMUM_HEIGHT)
 	draggable = false
 
+
 func _initialize_port() -> void:
 	_port = Control.new()
 	add_child(_port)
+
 
 func _initialize_content_vbox() -> void:
 	var margin_content_container = MarginContainer.new()
@@ -68,21 +71,22 @@ func _initialize_content_vbox() -> void:
 
 	_content_container = VBoxContainer.new()
 	_set_container_to_expand_fully(_content_container)
-	
+
 	margin_content_container.add_child(_content_container)
-	
+
 	add_child(margin_content_container)
 	_has_properties = false
-	
-	
+
+
 func _initialize_titlebar() -> void:
 	_titlebar = get_titlebar_hbox()
-	
-	
+
+
 func _initialize_titlebar_icon() -> void:
 	_titlebar_icon = TextureRect.new()
 	_titlebar.add_child(_titlebar_icon)
 	_titlebar.move_child(_titlebar_icon, 0)
+
 
 func _configure_ports(sense_node: SenseTreeNode) -> void:
 	var node_group: SenseTreeConstants.NodeGroup = sense_node.get_node_group()
@@ -131,54 +135,59 @@ func _configure_ports(sense_node: SenseTreeNode) -> void:
 			push_error()
 			return
 
+
 func _load_node_icon(sense_node: SenseTreeNode) -> void:
-	var sense_node_class: String = sense_node.get_sensenode_class()	
+	var sense_node_class: String = sense_node.get_sensenode_class()
 	if not sense_node_class or sense_node_class.is_empty():
 		push_warning("Could not resolve class name from SenseTree node %s." % sense_node.name)
 		return
-	
+
 	var icon_path: String = _try_acquire_icon_path(sense_node_class)
 	if not icon_path or icon_path.is_empty():
-		push_warning("Could not find matching icon for SenseTree node of type %s." % sense_node_class)
+		push_warning(
+			"Could not find matching icon for SenseTree node of type %s." % sense_node_class
+		)
 		return
-	
+
 	_node_icon_texture = load(icon_path)
 	_titlebar_icon.texture = _node_icon_texture
+
 
 func _load_node_title(sense_node: SenseTreeNode) -> void:
 	var title_label = Label.new()
 	title_label.text = sense_node.name
 	_titlebar.add_child(title_label)
 
+
 func _load_node_properties(sense_node: SenseTreeNode) -> void:
 	var properties: Array[SenseTreeExportedProperty] = sense_node.get_exported_properties()
-	
+
 	if not properties.is_empty():
 		_has_properties = true
-	
+
 	for property in properties:
-			var property_container = HBoxContainer.new()
-			_set_container_margins(property_container, PROPERTY_ITEM_MARGIN)
-			property_container.add_theme_constant_override("separation", 10)
-			
-			var name_label = Label.new()
-			name_label.text = "%s:" % property.property_title
-			property_container.add_child(name_label)
-			
-			var value_label = Label.new()
-			value_label.text = str(property.value)
-			property_container.add_child(value_label)
-			
-			_content_container.add_child(property_container)
+		var property_container = HBoxContainer.new()
+		_set_container_margins(property_container, PROPERTY_ITEM_MARGIN)
+		property_container.add_theme_constant_override("separation", 10)
+
+		var name_label = Label.new()
+		name_label.text = "%s:" % property.property_title
+		property_container.add_child(name_label)
+
+		var value_label = Label.new()
+		value_label.text = str(property.value)
+		property_container.add_child(value_label)
+
+		_content_container.add_child(property_container)
 
 
 func _try_acquire_icon_path(sense_node_class: String) -> String:
-	var all_classes: Array[Dictionary] = ProjectSettings.get_global_class_list()	
+	var all_classes: Array[Dictionary] = ProjectSettings.get_global_class_list()
 	for class_definition in all_classes:
 		if class_definition.has("class") and class_definition.get("class") == sense_node_class:
 			if class_definition.has("icon"):
 				return class_definition.get("icon")
-				
+
 	return String()
 
 
@@ -223,6 +232,7 @@ func _set_node_position(arranged_node: ArrangedVisualizerNode) -> void:
 	var y_position = (node_y_offset_units) * VERTICAL_SPACING_OFFSET
 	position_offset = Vector2(x_position, y_position)
 
+
 func _set_container_margins(container: Container, margin: int) -> void:
 	container.add_theme_constant_override("margin_top", margin)
 	container.add_theme_constant_override("margin_left", margin)
@@ -233,4 +243,3 @@ func _set_container_margins(container: Container, margin: int) -> void:
 func _set_container_to_expand_fully(container: Container) -> void:
 	container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	container.size_flags_vertical = Control.SIZE_EXPAND_FILL
-	
