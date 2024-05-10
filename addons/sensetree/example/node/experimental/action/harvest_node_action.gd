5e1ffdc2d77e1c241cda3ada5ffcddd621169fca
Benas Kiburtas <benas.kiburtas@pm.me>
2024-05-10 20:10:06 +0300

    Initial harvest node action implementation

--- addons/sensetree/example/node/experimental/action/harvest_node_action.gd
+++ addons/sensetree/example/node/experimental/action/harvest_node_action.gd
@@ -5,3 +5,14 @@ extends SenseTreeActionLeaf
 
 #Entity harvests a node and retrieves resources
 #Mainly for farmland, possibly applicable to wildlife corpses or other nodes (fishing?)
+
+@export var harvestable_nodes: Array[HarvestableNode]
+
+
+func tick(actor, blackboard):
+	for harvestable in harvestable_nodes:
+		if not harvestable.is_ready_for_harvest():
+			var harvest_result = harvestable.harvest()
+			if harvest_result:
+				return Status.SUCCESS
+	return Status.FAILURE
